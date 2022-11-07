bdb88b901d73bf704cc2fc502925c20952d284c5
Manthongkham <117598574+Manthongkham@users.noreply.github.com>
2022-11-06 22:08:56 -0600

    Add files via upload

---
+++ ascii_art.py
@@ -0,0 +1,17 @@
+'''
+Here is the ASCII art demonstrating a fish to satisfy COMP1900, Lab 1, Part 2, question 2
+'''
+
+
+print('       o                 o  ')
+print('                  o         ')
+print('         o   ______      o  ')
+print('           _/  (   \\_      ')
+print(' _       _/  (       \\_  O ')
+print('| \\_   _/  (   (    0  \\  ')
+print('|== \\_/  (   (          |  ')
+print('|=== _ (   (   (        |   ')
+print('|==_/ \\_ (   (          |  ')
+print('|_/     \\_ (   (    \\__/  ')
+print('            |  |___/        ')
+print('           /__/             ')
---
+++ fav_quotes.py
@@ -0,0 +1,18 @@
+'''
+These are my three favorite quotes!
+Top said that I am allow to have the same favorite quotes as the example
+'''
+
+#1. Write a program named fav_quotes.py that displays three of your favorite quotes, 
+#2. within double quotation marks
+#3. Each quote should be followed by a line sowing who said/wrote it
+#4. Skip one line between each quote.
+
+
+print()
+print('"And so even though we face the difficulties of today and tomorrow, I\nstill have a dream. It is a dream deeply rooted in the American dream."\n - Martin Luther King, Jr.')
+print()
+print('"Horns, horns, horns, in dark Mindolluin\'s sides they dimly echoed. Great\nhorns of the north wildly blowing. Rohan had come at last."\n - J.R.R Tolkien, The Return of the King')
+print()
+print('"A well-built physique is a status symbol. It reflects you worked hard\nfor it. No money can buy it. You cannot inherit it. You cannot steal it.\nYou cannot borrow it. You cannot hold on to it without constant work. It\nshows dedication. It shows discipline. It shows self-respect. It shows\ndignity. It shows patience, work ethic, passion. That is why it\'s \nattractive to me."\n - Pauline Nordin')
+print()
